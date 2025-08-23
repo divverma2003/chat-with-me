46435d56fd541b6790722ac9854c6a9afbb9e9a8
divverma2003 <divyamalikverma@gmail.com>
2025-08-23 11:11:22 -0400

    User schema created.

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = (req, res) => {
+  res.send("User registration");
+};
+
+export const login = (req, res) => {
+  res.send("User login");
+};
+
+export const logout = (req, res) => {
+  res.send("User logout");
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
 
+const PORT = process.env.PORT || 3000;
 const app = express();
-const APP_PORT = process.env.PORT || 5001;
 
-app.listen(APP_PORT, () => {
-  console.log(`Server is running on port ${APP_PORT}`);
+dotenv.config();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
