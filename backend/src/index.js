337762b756c678764e334e4d9ac09ac5b3b4d41f
divverma2003 <divyamalikverma@gmail.com>
2025-08-29 22:05:51 -0400

    Fixed CORS config for current express version

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,8 +23,9 @@ app.use("/api/messages", messageRoutes);
 // serve API and REACT under the same name
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 server.listen(APP_PORT, () => {
